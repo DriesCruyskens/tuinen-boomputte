0df69aef77612b26ac73bbc9d79c63a07648e31d
driescruyskens <dries.cruyskens@gmail.com>
2022-03-19 17:50:02 +0100

    font typescript type definition

---
+++ src/fonts/fonts.d.ts
@@ -0,0 +1,3 @@
+// This is here so importing fonts does not give error
+// https://www.designcise.com/web/tutorial/how-to-fix-cannot-find-module-error-when-importing-fonts-with-typescript
+declare module '*.otf';
