811e32882d8aaf444c6b615fed19731634799774
driescruyskens <dries.cruyskens@gmail.com>
2022-03-20 17:36:12 +0100

    added netlify cms config.yml, removed plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,19 @@ module.exports = {
     title: `tuinen boomputte`,
     siteUrl: `https://www.tuinenboomputte.be`
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  plugins: ["gatsby-plugin-netlify-cms", 
+            "gatsby-plugin-emotion", 
+            "gatsby-plugin-image", 
+            "gatsby-plugin-react-helmet", 
+            "gatsby-plugin-sitemap", 
+            "gatsby-plugin-sharp", 
+            "gatsby-transformer-sharp", {
+              resolve: 'gatsby-source-filesystem',
+              options: {
+                "name": "images",
+                "path": "./src/images/"
+              },
+              __key: "images"
+            }   
+]
 };
